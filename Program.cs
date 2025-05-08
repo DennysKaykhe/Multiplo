b9e0cef704d04ad087143f173f24bf765ac2fcda
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-08 19:54:06 -0300

    Eu amo números multiplos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double numero, multiplo;
+
+Console.Clear();
+
+Console.WriteLine("--- Múltiplo ---");
+
+Console.Write("\nDigite um número...........: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Avaliar se é múltiplo de...: ");
+multiplo = Convert.ToDouble(Console.ReadLine());
+
+if (numero % multiplo == 0)
+{
+    Console.Write($"\n{numero} é multiplo de {multiplo}");
+}
+else
+{
+    Console.Write($"\n{numero} não é multiplo de {multiplo}");
+}
